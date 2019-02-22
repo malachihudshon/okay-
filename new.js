5f771af6723d3f2398e71450a59333bdf4f72183
malachihudshon <37843505+malachihudshon@users.noreply.github.com>
2019-02-22 15:40:57 -0800

    Add files via upload

---
+++ new.js
@@ -0,0 +1,36 @@
+
+
+function nextImage(el){
+	if (el.src.match("imuy/cell.jpg")){
+		el.src="imuy/gok.png";
+	} else if (el.src.match("imuy/gok.png")){
+		el.src="imuy/cell.jpg";
+	} else {
+	}
+}
+
+function nextImag(el){
+	if (el.src.match("imuy/cell.jpg")){
+		el.src="imuy/ce.jpg";
+	} else if (el.src.match("imuy/ce.jpg")){
+		el.src="imuy/cell.jpg";
+	} else {
+	}
+}
+
+function nextIma(el){
+	if (el.src.match("imuy/images.png")){
+		el.src="imuy/gok.png";
+	} else if (el.src.match("imuy/gok.png")){
+		el.src="imuy/images.png";
+	} else {
+	}
+}
+function nextIm(el){
+	if (el.src.match("imuy/by.jpg")){
+		el.src="imuy/yam.jpg";
+	} else if (el.src.match("imuy/yam.jpg")){
+		el.src="imuy/by.jpg";
+	} else {
+	}
+}
